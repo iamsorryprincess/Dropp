ac954c3bee5aa7f9bad1037b38b90bd0aa0ccc90
PankratovNikita <NickDollars@yandex.ru>
2017-11-09 23:35:31 +0300

    Рефакторинг сервисов, about, постов и формы

--- scripts/TextBox.js
+++ scripts/TextBox.js
@@ -1,24 +1,24 @@
-window.onload = function(){
+window.onload = function() {
 	
-	function TextBox(options){
+	function TextBox(options) {
 		let field = options.field;
-		let text = options.text;
+		let hint = options.hint;
 		field.onfocus = focusOn;
 		field.onblur = focusOff;
 
-		function focusOn(){
-			text.classList.add("text-box__text_active");
+		function focusOn() {
+			hint.classList.add("contact__hint_active");
 		}
 
-		function focusOff(){
-			if (field.value === "") text.classList.remove("text-box__text_active");
+		function focusOff() {
+			if (field.value === "") hint.classList.remove("contact__hint_active");
 		}
 	}
 
-	var nameTextBox = new TextBox({field: document.getElementsByClassName("text-box__field")[0],
-								text: document.getElementsByClassName("text-box__text")[0]});
-	var emailTextBox = new TextBox({field: document.getElementsByClassName("text-box__field")[1],
-								text: document.getElementsByClassName("text-box__text")[1]});
-	var messageTextBox = new TextBox({field: document.getElementsByClassName("text-box__field")[2],
-								text: document.getElementsByClassName("text-box__text")[2]});
+	var nameTextBox = new TextBox({field: document.getElementsByClassName("contact__text-field")[0],
+								hint: document.getElementsByClassName("contact__hint")[0]});
+	var emailTextBox = new TextBox({field: document.getElementsByClassName("contact__text-field")[1],
+								hint: document.getElementsByClassName("contact__hint")[1]});
+	var messageTextBox = new TextBox({field: document.getElementsByClassName("contact__text-field")[2],
+								hint: document.getElementsByClassName("contact__hint")[2]});
 };
