817e7edb1407b9279c27762f93268fffedc080d2
PankratovNikita <NickDollars@yandex.ru>
2017-11-14 21:33:06 +0300

    отслеживание в Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,22 @@ var less = require('gulp-less');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('css', function() {
-  return gulp.src('css/*.less')
-    .pipe(less())
-    .pipe(gulp.dest('css'))
+	return gulp.src('css/*.less')
+	.pipe(less())
+	.pipe(gulp.dest('css'))
 });
 
-gulp.task('prefixes', function() {
-    gulp.src('css/style.css')
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('css'))
+gulp.task('prefixes', ['css'], function() {
+	gulp.src('css/style.css')
+	.pipe(autoprefixer({
+		browsers: ['last 2 versions'],
+		cascade: false
+	}))
+	.pipe(gulp.dest('css'))
 });
 
-gulp.task('default', ['prefixes']);
+gulp.task('watch', function () {
+	gulp.watch('css/*.less', ['css', 'prefixes']);
+});
+
+gulp.task('default', ['watch']);
